51036d0c349cd21e7e9a814e971de8ceaedd4a6d
Lukas Tongco <lukas.tongco@gmail.com>
2024-12-14 14:16:15 -0800

    Create POST and DEL routes

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -17,9 +17,39 @@ router.get('/', async (req, res) => {
 })
 
 // GET 'api/v1/todos/:id'
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const todo = await db.getTodoById(id)
+    res.json(todo)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
 
 // POST 'api/v1/todos/'
+router.post('/', async (req, res) => {
+  const newTodo = req.body
+  try {
+    await db.addTodo(newTodo)
+    res.sendStatus(200)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
 
 // DEL 'api/v1/todos/:id'
+router.delete('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    await db.deleteTodo(id)
+    res.sendStatus(200)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
 
 export default router
